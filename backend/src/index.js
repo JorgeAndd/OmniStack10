2fdf42d9ade4a6f4b31d7b3c08674fdbc6b261f1
JorgeAndd <jorgeluizandrade@outlook.com>
2020-01-14 20:52:26 -0300

    Add mongoose connection to local database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const routes = require('./routes');
 
 const app = express();
+
+const mongoUri = 'mongodb://localhost/omni?retryWrites=true&w=majority';
+
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose successfully connected to ' + mongoUri);
+});
+
+mongoose.connection.on('error', () => {
+    console.log('[Error] Mongoose could not connect to ' + mongoUri);
+});
 
 app.use(express.json());
 app.use(routes);
