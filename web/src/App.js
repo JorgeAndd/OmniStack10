80d5f4fac5ec45b8182f6ea62d767b0589047826
JorgeAndd <jorgeluizandrade@outlook.com>
2020-01-16 19:49:14 -0300

    Basic global styles and sidebar fields

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import './global.css';
+import './App.css';
 
 function App() {
-    return (
-        <h1>Hello World!</h1>
-    );
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username">Github User</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div class="input-block">
+            <label htmlFor="skills">Skills</label>
+            <input name="skills" id="skills" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Save</button>
+        </form>
+      </aside>
+
+      <main>
+
+      </main>
+    </div >
+  );
 }
 
 export default App;
