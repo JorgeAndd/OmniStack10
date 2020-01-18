bda7055ac301cd27c4390ec10c5f9a1a0385d927
JorgeAndd <jorgeluizandrade@outlook.com>
2020-01-18 15:23:05 -0300

    Add search inputs

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Main(props) {
     const [currentRegion, setCurrentRegion] = useState(null);
@@ -35,21 +36,36 @@ function Main(props) {
     }
 
     return (
-        <MapView initialRegion={currentRegion} style={styles.map}>
-            <Marker coordinate={{ latitude: -15.7541464, longitude: -47.9001824 }}>
-                <Image style={styles.avatar} source={{ uri: "https://avatars1.githubusercontent.com/u/5915194?s=460&v=4" }} />
+        <>
+            <MapView initialRegion={currentRegion} style={styles.map}>
+                <Marker coordinate={{ latitude: -15.7541464, longitude: -47.9001824 }}>
+                    <Image style={styles.avatar} source={{ uri: "https://avatars1.githubusercontent.com/u/5915194?s=460&v=4" }} />
 
-                <Callout onPress={() => {
-                    navigation.navigate('Profile', { github_username: 'JorgeAndd' });
-                }}>
-                    <View style={styles.callout}>
-                        <Text style={styles.devName}>Jorge Andrade</Text>
-                        <Text style={styles.devBio}>Mauris et turpis facilisis, blandit augue scelerisque, consequat felis. In quis auctor dolor.</Text>
-                        <Text style={styles.devSkills}>C#, Javascript, React</Text>
-                    </View>
-                </Callout>
-            </Marker>
-        </MapView>
+                    <Callout onPress={() => {
+                        navigation.navigate('Profile', { github_username: 'JorgeAndd' });
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.devName}>Jorge Andrade</Text>
+                            <Text style={styles.devBio}>Mauris et turpis facilisis, blandit augue scelerisque, consequat felis. In quis auctor dolor.</Text>
+                            <Text style={styles.devSkills}>C#, Javascript, React</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
+            <View style={styles.searchForm}>
+                <TextInput
+                    style={styles.searchInput}
+                    placeholder='Search devs by skill...'
+                    placeholderTextColor='#999'
+                    autoCapitalize='words'
+                    autoCorrect={false}
+                />
+
+                <TouchableOpacity onPress={() => { }} style={styles.loadButton}>
+                    <MaterialIcons name='my-location' size={20} color='#FFF' />
+                </TouchableOpacity>
+            </View>
+        </>
     );
 }
 
@@ -78,6 +94,39 @@ const styles = StyleSheet.create({
     devSkills: {
         marginTop: 5,
 
+    },
+    searchForm: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row',
+    },
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+        elevation: 2,
+    },
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8E4DFF',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15,
     }
 
 });
