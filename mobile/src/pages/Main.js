4065e7d9e9cc6a08872e4fe3573ecde86de64690
JorgeAndd <jorgeluizandrade@outlook.com>
2020-01-18 15:13:13 -0300

    Show github profile page when clicking on a dev

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, Image, View, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
-function Main() {
+function Main(props) {
     const [currentRegion, setCurrentRegion] = useState(null);
+    const { navigation } = props;
 
     useEffect(() => {
         async function loadInitialPosition() {
@@ -37,6 +38,16 @@ function Main() {
         <MapView initialRegion={currentRegion} style={styles.map}>
             <Marker coordinate={{ latitude: -15.7541464, longitude: -47.9001824 }}>
                 <Image style={styles.avatar} source={{ uri: "https://avatars1.githubusercontent.com/u/5915194?s=460&v=4" }} />
+
+                <Callout onPress={() => {
+                    navigation.navigate('Profile', { github_username: 'JorgeAndd' });
+                }}>
+                    <View style={styles.callout}>
+                        <Text style={styles.devName}>Jorge Andrade</Text>
+                        <Text style={styles.devBio}>Mauris et turpis facilisis, blandit augue scelerisque, consequat felis. In quis auctor dolor.</Text>
+                        <Text style={styles.devSkills}>C#, Javascript, React</Text>
+                    </View>
+                </Callout>
             </Marker>
         </MapView>
     );
@@ -51,8 +62,24 @@ const styles = StyleSheet.create({
         height: 40,
         borderRadius: 50,
         borderWidth: 2,
-        borderColor: "#7D40E7",
+        borderColor: '#7D40E7',
+    },
+    callout: {
+        width: 260,
+    },
+    devName: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    devBio: {
+        color: '#666',
+        marginTop: 5,
+    },
+    devSkills: {
+        marginTop: 5,
+
     }
+
 });
 
 export default Main;
